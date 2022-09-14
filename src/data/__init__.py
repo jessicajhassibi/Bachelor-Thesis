2507d05425747a13e656707a6112e334c6fc0629
Jessica Hassibi <s6653550@stud.uni-frankfurt.de>
2022-09-14 12:18:37 +0200

    Update data_preparation

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,3 +2,4 @@ from .data_analysis import language_analyzer
 from .data_analysis import plot_scraped_data
 from .models import Group
 from .wiki_scraper import scrape_and_generate_data
+from .nlp import create_dataframes, create_cleaned_dataframes

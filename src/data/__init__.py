34def0c55234dee7b1c68c9d446e766086121c7c
Oweis Al-Agtash <oweis.al-agtash@objective-partner.com>
2022-09-14 09:26:10 +0200

    Checkpoint 4: data analysis DONE
    Signed-off-by: Oweis Al-Agtash <oweis.al-agtash@objective-partner.com>

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 from .data_analysis import language_analyzer
+from .data_analysis import plot_scraped_data
 from .models import Group
 from .wiki_scraper import scrape_and_generate_data
--- src/data/data_analysis.py
+++ src/data/data_analysis.py
@@ -1,8 +1,8 @@
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 
-from .data_helpers import get_groups, get_dataframe_from_json, get_json_target_path
+from .data_helpers import get_groups, get_dataframe_from_json, get_json_target_path, get_target_path
+from .models import Group
 
 
 def text_counter(texts_array):
@@ -24,7 +24,12 @@ def text_counter(texts_array):
     return a, p, w
 
 
-def language_analyzer():
+ARTICLES_STR = "articles"
+PARAGRAPHS_STR = "paragraphs"
+WORDS_STR = "words"
+
+
+def language_analyzer(group_0: Group, group_1: Group):
     """
     Creates nested dict of languages and their counts of articles, paragraphs, words and categories.
     """
@@ -32,66 +37,50 @@ def language_analyzer():
     group_0_count_dict: dict = dict()
     group_1_count_dict: dict = dict()
 
-    article_count, paragraph_count, words_count = 0, 0, 0
-    group_0, group_1 = get_groups()
+    for group, count_dict in [(group_0, group_0_count_dict), (group_1, group_1_count_dict)]:
+        count_dict[group.label] = dict()
+        for lang in group.wiki_languages:
+            article_count, paragraph_count, words_count = 0, 0, 0
+            json_path = get_json_target_path(group.wiki_page, group.label, lang)
+            df = get_dataframe_from_json(str(json_path))
+            counts = text_counter(df['text'])
+            article_count += counts[0]
+            paragraph_count += counts[1]
+            words_count += counts[2]
+            count_dict[group.label][lang] = {ARTICLES_STR: article_count,
+                                             PARAGRAPHS_STR: paragraph_count,
+                                             WORDS_STR: words_count}
 
-    # ---------- group 0 -----------
-    for lang in group_0.wiki_languages:
-        json_path = get_json_target_path(group_0.wiki_page, group_0.label, lang)
-        df_0 = get_dataframe_from_json(str(json_path))
-        counts = text_counter(df_0['text'])
-        article_count += counts[0]
-        paragraph_count += counts[1]
-        words_count += counts[2]
-        group_0_count_dict[group_0.label][lang] = {"articles": article_count,
-                                                   "paragraphs": paragraph_count,
-                                                   "words": words_count}
-
-    # ---------- group 1 -----------
-    article_count, paragraph_count, words_count = 0, 0, 0
-    for lang in group_1.wiki_languages:
-        json_path = get_json_target_path(group_1.wiki_page, group_1.label, lang)
-        df_1 = get_dataframe_from_json(str(json_path))
-        counts = text_counter(df_1['text'])
-        article_count += counts[0]
-        paragraph_count += counts[1]
-        words_count += counts[2]
-        group_1_count_dict[group_1.label][lang] = {"articles": article_count,
-                                                   "paragraphs": paragraph_count,
-                                                   "words": words_count}
-
-    print("group_0_count_dict=", group_0_count_dict)
+    print("group_0_count_dict=", count_dict)
     print("group_1_count_dict=", group_1_count_dict)
     return group_0_count_dict, group_1_count_dict
 
 
-def jupyter():
-    # 3 Pie charts to be side by side
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 20))
-
+def plot_pie_chart(group: Group, group_count_dict: dict, element: str, ax):
     # Plotting articles pie chart
-    sizes = [languages_count_dict[lang]["articles"] for lang in languages_count_dict.keys()]
-    ax1.pie(sizes, labels=data_analysis.get_languages(), autopct=lambda pct: pct_func(pct, sizes))
-    ax1.set_title("Verteilung der Artikel")
+    sizes = [group_count_dict[group.label][lang][element] for lang in group.wiki_languages]
+    ax.pie(sizes, labels=group.wiki_languages, autopct=lambda pct: pct_func(pct, sizes))
+    ax.set_title(f"{group.label} - Distribution of {element}")
 
-    # Plotting paragraphs pie chart
-    sizes = [languages_count_dict[lang]["paragraphs"] for lang in languages_count_dict.keys()]
-    ax2.pie(sizes, labels=languages, autopct=lambda pct: pct_func(pct, sizes))
-    ax2.set_title("Verteilung der Paragraphen")
 
-    # Plotting words pie chart
-    sizes = [languages_count_dict[lang]["words"] for lang in languages_count_dict.keys()]
-    ax3.pie(sizes, labels=languages, autopct=lambda pct: pct_func(pct, sizes))
-    ax3.set_title("Verteilung der Wörter")
+def plot_scraped_data():
+    group_0, group_1 = get_groups()
+    group_0_count_dict, group_1_count_dict = language_analyzer(group_0, group_1)
+    # 3 Pie charts to be side by side
+    for group, count_dict in [(group_0, group_0_count_dict), (group_1, group_1_count_dict)]:
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 20))
+        for elem, ax in [(ARTICLES_STR, ax1), (PARAGRAPHS_STR, ax2), (WORDS_STR, ax3)]:
+            plot_pie_chart(group, count_dict, elem, ax)
+            # get current figure
+        fig_out = plt.gcf()
 
-    # get current figure
-    fig_out = plt.gcf()
-
-    # show plot
-    plt.show()
+        # show plot
+        plt.show()
 
     # save plot
-    fig_out.savefig("../reports/figures/Sprachverteilungen.pdf")
+    target = get_target_path().joinpath("reports/figures").resolve()
+    target.mkdir(parents=True, exist_ok=True)
+    fig_out.savefig(target.joinpath("Sprachverteilungen.pdf"))
 
 
 def pct_func(pct, allvalues):
